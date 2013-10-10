0b997c9ec0007dacb726081573ff3031afdc186c
heartsentwined <heartsentwined.me@gmail.com>
2013-10-10 12:40:10 +0800

    built dist files

--- dist/ember-auth-session-local-storage.js
+++ dist/ember-auth-session-local-storage.js
@@ -3,7 +3,7 @@ var get$ = Ember.get;
 Em.onLoad('Ember.Application', function (application) {
   return application.initializer({
     name: 'ember-auth.session.local-storage',
-    after: 'ember-auth',
+    before: 'ember-auth-load',
     initialize: function (container, app) {
       return app.register('authSession:localStorage', get$(get$(Em, 'Auth'), 'LocalStorageAuthSession'));
     }
